c1b16c10ea635ea3a9c101389130297b49556b69
Vincent Baquiro <vincent.baquiro@dynatacorp.com>
2023-03-24 09:40:47 -0400

    commite

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import logo from "../images/logo.jpg";
 export const HeaderMenu = () => {
   return (
     <header>
-      <nav className="navbar mt-5  navbar-expand-md">
+      <nav id="nav-main" className="navbar pt-5 pt-lg-5 pt navbar-expand-md">
         <img className="logo mx-auto" src={logo} alt="logo" />
         <div className="container-fluid mt-lg-4">
           <span></span>
@@ -40,3 +40,40 @@ export const HeaderMenu = () => {
     </header>
   );
 };
+
+{
+  /* <nav class="navbar navbar-expand-md fixed-top">
+  <div class="container-fluid mt-lg-4">
+    <a class="navbar-brand" href="#">
+      Brand
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarCollapse"
+      aria-controls="navbarCollapse"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div
+      class="collapse navbar-collapse justify-content-center"
+      id="navbarCollapse"
+    >
+      <ul className="navbar-nav">
+        {NavList.map((list) => {
+          return (
+            <li className="nav-item">
+              <Link to={list.links} className="nav-link">
+                {list.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  </div>
+</nav>; */
+}
